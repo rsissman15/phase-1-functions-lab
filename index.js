7b4a26f951c0f733f1e457ce4d87236a8ced8fba
Rebecca Sissman <rsissman15@aol.com>
2022-01-10 17:48:41 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+   const distanceFromHqInBlocks=Math.abs(pickup-42);
+    return distanceFromHqInBlocks;
+}
+
+function distanceFromHqInFeet(pickup){
+   const distanceFromHqInFeet= Math.abs(pickup-42)*264;
+   return distanceFromHqInFeet;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const distanceTraveledInFeet= Math.abs(start-destination)*264;
+    return distanceTraveledInFeet;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceTraveled= Math.abs(start-destination)*264;
+
+    if (distanceTraveled<400){
+        let cost=0;
+        return cost;
+    }
+    else if (distanceTraveled>=400 && distanceTraveled<2000){
+        let cost=(0.02)*(distanceTraveled-400);
+        return cost;
+    
+    }
+    else if(distanceTraveled>=2500){
+        return "cannot travel that far";
+    }
+    else if(distanceTraveled>=2000 && distanceTraveled<2500){
+        let cost=25;
+        return cost;
+
+    }
+}
